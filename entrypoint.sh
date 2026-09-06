03256517d6aec93aa944757981af5e06147283b4
Cody Chapman <cody.chapman@gmail.com>
2026-09-06 09:56:47 -0500

    Refactor Postfix entrypoint configuration
    Updated default values for MYHOSTNAME, MYNETWORKS, and RELAYHOST. Added SMTP server header checks and increased email size limits.

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,11 +24,9 @@ printenv > /etc/environment
 mkdir -p /var/spool/postfix /var/log/postfix
 
 # Fallback defaults if variables are not passed to the container
-MYHOSTNAME="${MYHOSTNAME:-vnatoday.org}"
-MYNETWORKS="${MYNETWORKS:-192.168.0.0/24 172.16.0.0/24}"
-RELAYHOST="${RELAYHOST:-[://outlook.com]:25}"
-
-echo "--> Generating main.cf dynamically via EOF..."
+MYHOSTNAME="${MYHOSTNAME:-westgate.bank}"
+MYNETWORKS="${MYNETWORKS:-10.0.0.0/8}"
+RELAYHOST="${RELAYHOST:-[westgate-bank.mail.protection.outlook.com]:25}"
 
 # Overwrite /etc/postfix/main.cf dynamically
 cat << EOF > /etc/postfix/main.cf
@@ -36,12 +34,15 @@ cat << EOF > /etc/postfix/main.cf
 inet_interfaces = all
 inet_protocols = ipv4
 
+# setting sane defaults
+default_database_type = lmdb
+
 # Identity
 myhostname = ${MYHOSTNAME}
 mydomain = ${MYHOSTNAME}
 myorigin = \$mydomain
 
-smtpd_banner = \$myhostname ESMTP \$mail_name
+smtpd_banner = ESMTP
 
 # Do not deliver locally
 mydestination =
@@ -56,6 +57,9 @@ compatibility_level = 3.6
 # Relay Everything Through Microsoft 365
 relayhost = ${RELAYHOST}
 
+# SMTP Server header check hardening
+header_checks = pcre:/etc/postfix/header_checks
+
 # SMTP Server Restrictions
 smtpd_relay_restrictions = 
     permit_mynetworks, 
@@ -64,18 +68,68 @@ smtpd_relay_restrictions =
 smtpd_recipient_restrictions = 
     permit_mynetworks, 
     reject_unauth_destination
+
+# Set TLS up for connectivity
+smtpd_tls_security_level = may
+smtpd_tls_cert_file = /etc/ssl/certs/postfix.pem
+smtpd_tls_key_file = /etc/ssl/private/postfix.key
+smtp_tls_security_level = encrypt
+# Following 2 settings are deprecated
+#smtp_enforce_tls = yes
+#smtp_use_tls = yes
+smtp_tls_loglevel = 1
+
+# Increase max size of emails and mailbox
+message_size_limit = 52428800
+mailbox_size_limit = 104857600
 EOF
 
-echo "--> Configuration file created successfully."
+echo "--> Configuration file [main.cf] created successfully."
 
 chown root:root /etc/postfix/main.cf
+
+
+cat << EOF > /etc/postfix/header_checks
+# Remove internal client Received headers (originating client info)
+/^Received: from .* \(HELO .*\) \([^)]+\) by yourdomain\.com/ IGNORE
+/^Received: from .* \[127\.0\.0\.1\]/ IGNORE
+/^Received: from .* \[10\./ IGNORE
+/^Received: from .* \[192\.168\./ IGNORE
+
+# Remove client user agent, X-Mailer, and intermediate origin headers
+/^User-Agent:/ IGNORE
+/^X-Mailer:/ IGNORE
+/^X-Originating-IP:/ IGNORE
+/^X-Enigmail:/ IGNORE
+/^X-PHP-Originating-Script:/ IGNORE
+EOF
+
+echo "--> Configuration file [header_checks] created successfully."
+
+chown root:root /etc/postfix/header_checks
+
+# Fixing issue with RHEL 10 default for lmdb vs hash
+postconf -e "alias_maps = lmdb:/etc/aliases"
+postconf -e "alias_database = lmdb:/etc/aliases"
+newaliases
+
+# Create tls certs to send emails securely
+mkdir -p /etc/ssl/certs /etc/ssl/private
+openssl req -new -x509 -nodes -days 365 \
+  -out /etc/ssl/certs/postfix.pem \
+  -keyout /etc/ssl/private/postfix.key \
+  -subj "/C=US/ST=Nebraska/L=Omaha/O=Visiting Nurse Association/OU=Information Technology/CN=vnatoday.org"
+
+chmod 600 /etc/ssl/private/postfix.key
+chmod 644 /etc/ssl/certs/postfix.pem
+
 
 # ----------------------------
 # Validate configuration
 # ----------------------------
-echo "Checking Postfix configuration..."
+echo "Checking Postfix [main.cf] configuration..."
 postfix check || {
-  echo "ERROR: Postfix configuration invalid"
+  echo "ERROR: Postfix configuration [main.cf] invalid"
   exit 1
 }
 
@@ -83,4 +137,3 @@ echo "--> Starting Postfix daemon in foreground..."
 
 # Start Postfix in foreground so the container stays alive
 exec /usr/sbin/postfix start-fg
-
